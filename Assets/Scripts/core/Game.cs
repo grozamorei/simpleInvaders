206e256564cc12ac0a7b1185392683667555ce18
grozamorei <desperatebassplayer@gmail.com>
2015-07-13 18:48:53 +0300

    player flicker

--- Assets/Scripts/core/Game.cs
+++ Assets/Scripts/core/Game.cs
@@ -5,10 +5,13 @@ public class Game : MonoBehaviour
 {
     public Rect border = new Rect(1, 3, 10, 5);
     public GameObject grid;
+    [SerializeField] private Transform[] _lifeIndicators;
     private Grid _grid;
+    public int currentLifes { get; private set; }
     
     void Awake()
     {
+        currentLifes = _lifeIndicators.Length;
         var g = Instantiate(grid);
         g.transform.SetParent(transform, false);
         g.transform.position = new Vector3(border.x, border.y + border.height, 0);
@@ -17,6 +20,26 @@ public class Game : MonoBehaviour
     
     void Update()
     {
+    }
+    
+    public void subtractLife()
+    {
+        currentLifes -= 1;
+        for (int i = 0; i < _lifeIndicators.Length; i++) {
+            if (_lifeIndicators[i] != null) {
+                Destroy(_lifeIndicators[i].gameObject);
+                break;
+            }
+        }
+        
+        if (currentLifes == -1) {
+            end();
+        }
+    }
+    
+    public void end()
+    {
+    
     }
 
     void OnDrawGizmos()
--- Assets/Scripts/core/player/Player.cs
+++ Assets/Scripts/core/player/Player.cs
@@ -10,14 +10,29 @@ public class Player : MonoBehaviour {
     [SerializeField] private float fasterEmission = 100;
     
     [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private Game _game;
+    [SerializeField] private int leftBound = 1;
+    [SerializeField] private int rightBound = 10;
+    
+    [SerializeField] private int flickerTime = 2;
+    [SerializeField] private int flickerSpeed = 1;
     
     private bool _leftDown = false;
     private bool _rightDown = false;
+    
+    private bool _flicker = false;
+    private float _currentTime = 0;
+    private int direction = -1;
+    private Material _material;
+    private PolygonCollider2D _collider;
     
     void Awake()
     {
         leftP.emissionRate = baseEmission;
         rightP.emissionRate = baseEmission;
+        
+        _material = GetComponent<SpriteRenderer>().material;
+        _collider = GetComponent<PolygonCollider2D>();
     }
     
     void Update()
@@ -47,7 +62,37 @@ public class Player : MonoBehaviour {
         }
         
         var pos = transform.position;
+        if (pos.x + currentVel >= rightBound) {
+            return;
+        }
+        if (pos.x + currentVel <= leftBound) {
+            return;
+        }
         transform.position = new Vector3(pos.x + currentVel, pos.y, pos.z);
+        
+        if (_flicker) {
+            _currentTime += Time.deltaTime;
+            
+            if (_currentTime >= flickerTime) {
+                _flicker = false;
+                _collider.enabled = true;
+                return;
+            }
+            
+            var c = _material.GetColor("_Color");
+            if (direction == -1) {
+                c.a -= Time.deltaTime * flickerSpeed;
+                if (c.a <= 0.2) {
+                    direction = 1;
+                }
+            } else {
+                c.a += Time.deltaTime * flickerSpeed;
+                if (c.a >= 1) {
+                    direction = -1;
+                }
+            }
+            _material.SetColor("_Color", c);
+        }
     }
     
     void OnCollisionEnter2D (Collision2D col)
@@ -64,5 +109,22 @@ public class Player : MonoBehaviour {
         var ps = Instantiate(explosion);
         ps.transform.position = new Vector3(midPoint.x, midPoint.y, -3);
         ps.Play();
+        
+        _game.subtractLife();
+        if (_game.currentLifes == 0) {
+            Destroy(gameObject);
+        } else {
+            _flicker = true;
+            _currentTime = 0;
+            _collider.enabled = false;
+        }
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        
+        Gizmos.DrawLine(new Vector3(leftBound, 0, 0), new Vector3(leftBound, 2, 0));
+        Gizmos.DrawLine(new Vector3(rightBound, 0, 0), new Vector3(rightBound, 2, 0));
     }
 }
