e6dd1c68745ec20056b6f3cf4bf64e64cebbc9b0
grozamorei <desperatebassplayer@gmail.com>
2015-07-14 03:41:47 +0300

    added some audio clips

---
+++ Assets/Scripts/util/Audio.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audio : MonoBehaviour {
+
+    
+
+    public void explode()
+    {
+    
+    }
+    
+    public void shoot()
+    {
+    
+    }
+}
